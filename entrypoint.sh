5bcf09f7ebd560da2901ff45e60c0f91603d8e6b
nahu02 <nahu1002@gmail.com>
2023-10-24 22:47:39 +0200

    updated to suit my needs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/sh -l
 
-THEME=${1}
-RESUME=${2}
-OUTPUT=${3}
+RESUME=${1}
+OUTPUT=${2}
 
-THEME_PACKAGE=jsonresume-theme-${THEME}
-echo "Installing theme: ${THEME}"
+THEME_PACKAGE=https://github.com/nahu02/jsonresume-theme-Abelstackoverflow
+echo "Installing theme: abelstackoverflow"
 
 # NOTE: this needs to be installed locally, not globally
 npm install ${THEME_PACKAGE}
 
-resume export --resume ${RESUME} --theme ${THEME} --format html ${OUTPUT}
+resume export --resume ${RESUME} --theme abelstackoverflow ${OUTPUT}
